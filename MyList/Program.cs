b440f9296fc8e76d26f1ebc6f816bcf47f716b52
mihasikk97 <mihasikk97@gmail.com>
2019-09-04 20:54:14 +0300

    MyListLib

--- MyList/Program.cs
+++ MyList/Program.cs
@@ -3,155 +3,156 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyListLib;
 
 namespace MyList
 {
-    class MyList
-    {
-        private int LastIndex { get; set; }
-        private object[] _array;
-        private int _capacity = 5;
+    //class MyList
+    //{
+    //    private int LastIndex { get; set; }
+    //    private object[] _array;
+    //    private int _capacity = 5;
 
-        public MyList(int capacity)
-        {
-            _capacity = capacity;
-            InitArray();
-        }
-        public MyList()
-        {
-            InitArray();
-        }
+    //    public MyList(int capacity)
+    //    {
+    //        _capacity = capacity;
+    //        InitArray();
+    //    }
+    //    public MyList()
+    //    {
+    //        InitArray();
+    //    }
 
 
-        //private void InitArray() => _array = new object[_capacity];
+    //    //private void InitArray() => _array = new object[_capacity];
 
-        private void InitArray()
-        {
+    //    private void InitArray()
+    //    {
 
-            _array = new object[_capacity];
+    //        _array = new object[_capacity];
 
-        }
-        private void RenitArray()
-        {
-            object[] newArray = _array;
-            _array = new object[_capacity];
-            if (_array != null)
-            {
-                for (int i = 0; i < newArray.Length; i++)
-                {
-                    _array[i] = newArray[i];
-                }
-            }
-        }
+    //    }
+    //    private void RenitArray()
+    //    {
+    //        object[] newArray = _array;
+    //        _array = new object[_capacity];
+    //        if (_array != null)
+    //        {
+    //            for (int i = 0; i < newArray.Length; i++)
+    //            {
+    //                _array[i] = newArray[i];
+    //            }
+    //        }
+    //    }
 
-        public void Add(int newValue)
-        {
-            if (LastIndex == _capacity)
-            {
-                _capacity *= 2;
-                RenitArray();
-            }
-            _array[LastIndex] = newValue;
-            LastIndex++;
-        }
+    //    public void Add(int newValue)
+    //    {
+    //        if (LastIndex == _capacity)
+    //        {
+    //            _capacity *= 2;
+    //            RenitArray();
+    //        }
+    //        _array[LastIndex] = newValue;
+    //        LastIndex++;
+    //    }
 
-        public void Insert(int index, object obj)
-        {
-            if (index <= _array.Length && index > 0)
-            {
-                if ((index == _capacity && index == LastIndex) || LastIndex == _capacity)
-                {
-                    _capacity *= 2;
-                    RenitArray();
-                }
+    //    public void Insert(int index, object obj)
+    //    {
+    //        if (index <= _array.Length && index > 0)
+    //        {
+    //            if ((index == _capacity && index == LastIndex) || LastIndex == _capacity)
+    //            {
+    //                _capacity *= 2;
+    //                RenitArray();
+    //            }
 
-                for (int i = _capacity -1 ; i > index; i--)
-                {
-                    _array[i] = _array[i - 1];
-                }
-                _array[index] = obj;
-                LastIndex++;
-            }
-            else
-            {
-                Console.WriteLine($"Inset false!!!"); ;
-            }
+    //            for (int i = _capacity -1 ; i > index; i--)
+    //            {
+    //                _array[i] = _array[i - 1];
+    //            }
+    //            _array[index] = obj;
+    //            LastIndex++;
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine($"Inset false!!!"); ;
+    //        }
 
-        }
+    //    }
 
-        public void Remove(object obj)
-        {
-            RemoveAt(IndexOf(obj));
+    //    public void Remove(object obj)
+    //    {
+    //        RemoveAt(IndexOf(obj));
             
-        }
-        public void RemoveAt(int index)
-        {
-            if (index < _array.Length && index > 0)
-            {
-                LastIndex--;
-                for(int i = index;i<LastIndex;i++)
-                {
-                    _array[i] = _array[i + 1];
-                }
-                _array[LastIndex] = null;
-            }
-            else
-            {
-                Console.WriteLine($"Remove false!!!");
-            }
-        }
+    //    }
+    //    public void RemoveAt(int index)
+    //    {
+    //        if (index < _array.Length && index > 0)
+    //        {
+    //            LastIndex--;
+    //            for(int i = index;i<LastIndex;i++)
+    //            {
+    //                _array[i] = _array[i + 1];
+    //            }
+    //            _array[LastIndex] = null;
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine($"Remove false!!!");
+    //        }
+    //    }
 
-        public void Clear()
-        {
-            for (int i = 0; i < _array.Length; i++)
-            {
-                _array[i] = null;
-            }
-        }
+    //    public void Clear()
+    //    {
+    //        for (int i = 0; i < _array.Length; i++)
+    //        {
+    //            _array[i] = null;
+    //        }
+    //    }
 
-        public bool Contains(object obj)
-        {
-            for (int i = 0; i < _array.Length; i++)
-            {
-                if ( Equals( _array[i], obj)) return true;
-            }
-            return false;
-        }
+    //    public bool Contains(object obj)
+    //    {
+    //        for (int i = 0; i < _array.Length; i++)
+    //        {
+    //            if ( Equals( _array[i], obj)) return true;
+    //        }
+    //        return false;
+    //    }
 
-        public int IndexOf(object obj)
-        {
-            for (int i = 0; i < _array.Length; i++)
-            {
-                if (Equals(_array[i], obj))
-                {
-                    return i; 
-                }
+    //    public int IndexOf(object obj)
+    //    {
+    //        for (int i = 0; i < _array.Length; i++)
+    //        {
+    //            if (Equals(_array[i], obj))
+    //            {
+    //                return i; 
+    //            }
 
-            }
-            return -1;
-        }
+    //        }
+    //        return -1;
+    //    }
 
 
-        public object[] ToArray()
-        {
-            object[] newArray = new object[_array.Length];
-            for (int i = 0; i < _array.Length; i++)
-            {
-                newArray[i] = _array[i];
-            }
-            return newArray;
-        }
+    //    public object[] ToArray()
+    //    {
+    //        object[] newArray = new object[_array.Length];
+    //        for (int i = 0; i < _array.Length; i++)
+    //        {
+    //            newArray[i] = _array[i];
+    //        }
+    //        return newArray;
+    //    }
 
-        public void Reverse()
-        {
-            for (int i = 0, j = _array.Length - 1; i < j; i++, j--)
-            {
-                object temp = _array[i];
-                _array[i] = _array[j];
-                _array[j] = temp;
-            }
-        }
-    }
+    //    public void Reverse()
+    //    {
+    //        for (int i = 0, j = _array.Length - 1; i < j; i++, j--)
+    //        {
+    //            object temp = _array[i];
+    //            _array[i] = _array[j];
+    //            _array[j] = temp;
+    //        }
+    //    }
+    //}
 
 
 
@@ -160,7 +161,7 @@ namespace MyList
         static void Main(string[] args)
         {
 
-            MyList array = new MyList();
+            MyListLib.MyListLib array = new MyListLib.MyListLib();
             array.Add(4);
             array.Add(7);
             array.Add(9);
---
+++ MyListLib/MyListLib.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyListLib
+{
+    public class MyListLib
+    {
+
+        private int LastIndex { get; set; }
+        private object[] _array;
+        private int _capacity = 5;
+
+        public MyListLib(int capacity)
+        {
+            _capacity = capacity;
+            InitArray();
+        }
+        public MyListLib()
+        {
+            InitArray();
+        }
+
+
+        //private void InitArray() => _array = new object[_capacity];
+
+        private void InitArray()
+        {
+
+            _array = new object[_capacity];
+
+        }
+        private void RenitArray()
+        {
+            object[] newArray = _array;
+            _array = new object[_capacity];
+            if (_array != null)
+            {
+                for (int i = 0; i < newArray.Length; i++)
+                {
+                    _array[i] = newArray[i];
+                }
+            }
+        }
+
+        public void Add(int newValue)
+        {
+            if (LastIndex == _capacity)
+            {
+                _capacity *= 2;
+                RenitArray();
+            }
+            _array[LastIndex] = newValue;
+            LastIndex++;
+        }
+
+        public void Insert(int index, object obj)
+        {
+            if (index <= _array.Length && index > 0)
+            {
+                if ((index == _capacity && index == LastIndex) || LastIndex == _capacity)
+                {
+                    _capacity *= 2;
+                    RenitArray();
+                }
+
+                for (int i = _capacity - 1; i > index; i--)
+                {
+                    _array[i] = _array[i - 1];
+                }
+                _array[index] = obj;
+                LastIndex++;
+            }
+            else
+            {
+                Console.WriteLine($"Inset false!!!"); ;
+            }
+
+        }
+
+        public void Remove(object obj)
+        {
+            RemoveAt(IndexOf(obj));
+
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < _array.Length && index > 0)
+            {
+                LastIndex--;
+                for (int i = index; i < LastIndex; i++)
+                {
+                    _array[i] = _array[i + 1];
+                }
+                _array[LastIndex] = null;
+            }
+            else
+            {
+                Console.WriteLine($"Remove false!!!");
+            }
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = null;
+            }
+        }
+
+        public bool Contains(object obj)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (Equals(_array[i], obj)) return true;
+            }
+            return false;
+        }
+
+        public int IndexOf(object obj)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (Equals(_array[i], obj))
+                {
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+
+
+        public object[] ToArray()
+        {
+            object[] newArray = new object[_array.Length];
+            for (int i = 0; i < _array.Length; i++)
+            {
+                newArray[i] = _array[i];
+            }
+            return newArray;
+        }
+
+        public void Reverse()
+        {
+            for (int i = 0, j = _array.Length - 1; i < j; i++, j--)
+            {
+                object temp = _array[i];
+                _array[i] = _array[j];
+                _array[j] = temp;
+            }
+        }
+    }
+}
